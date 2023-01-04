305d533aa3e44f4eb0c7e2647370643a35ec93c7
julianaflima <juli.faccio@gmail.com>
2023-01-04 08:26:26 +0530

    Add click and drag feature

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,13 @@
-const gridSide = 5;
-const drawSpace = document.querySelector('.draw-space');
+let gridSide = 4; //'';
+const drawSpace = document.querySelector('#draw-space');
+
+// do {
+// 	gridSide = +prompt('Choose a number between 1 and 100:');
+// } while (gridSide > 100 || gridSide < 1) 
 
 function createGrid(gridSide) {
 	let gridNumber = gridSide * gridSide;
-	let side = (500 - (gridSide + 1)) / gridSide;
+	let side = 500 / gridSide;
 
 	for(let i = 0; i < gridNumber; i++){
 		
@@ -16,3 +20,32 @@ function createGrid(gridSide) {
 }
 
 createGrid(gridSide);
+
+const squares = document.querySelectorAll('.inside-square')
+
+// Change color when square is clicked or over with click
+squares.forEach(square => {
+	square.addEventListener('mousedown', clickNDrag);
+});
+
+function clickNDrag(e) {
+	changeColor(e);
+	squares.forEach(square => {
+		square.addEventListener('mouseover', changeColor);
+	});
+	squares.forEach(square => {
+		square.addEventListener('mouseup', stop);
+	});
+}
+
+function changeColor(e) {
+	e.target.style.backgroundColor = 'red';
+}
+
+function stop(e) {
+	squares.forEach(square => {
+		square.removeEventListener('mouseover', changeColor);
+	});
+}
+
+
